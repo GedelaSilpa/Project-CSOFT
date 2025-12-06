16a0ec3f07056dd4a239d6163b9ded75d895c1a2
silpagedela <silpa.gedela@chandusoft.com>
2025-12-06 11:13:54 +0530

    version 3

--- project-root/app/db.php
+++ project-root/app/db.php
@@ -3,32 +3,6 @@
 // -----------------------------
 // Load .env manually
 // -----------------------------
-
-$envPath = dirname(__DIR__) . '/.env';   // Correct path: project-root/.env
-
-if (file_exists($envPath)) {
-
-    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-    foreach ($lines as $line) {
-
-        $line = trim($line);
-        if ($line === '' || str_starts_with($line, '#')) continue;
-
-        if (strpos($line, '=') !== false) {
-            list($key, $value) = explode('=', $line, 2);
-
-            $key = trim($key);
-            $value = trim($value, " \t\n\r\0\x0B\"'");
-
-            putenv("$key=$value");
-            $_ENV[$key] = $value;
-            $_SERVER[$key] = $value;
-        }
-    }
-}
-
-
 // -----------------------------
 // Custom env() function
 // -----------------------------
